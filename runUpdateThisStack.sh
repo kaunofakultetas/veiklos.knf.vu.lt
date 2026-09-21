ee4ebdbf1ccc6901612ebb590ebb3531b8598570
Tomas Vanagas <tomasvanagas@ymail.com>
2026-09-21 20:08:35 +0000

    Improve setup

--- runUpdateThisStack.sh
+++ runUpdateThisStack.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-mkdir -p ./_DATA/postgres
-mkdir -p ./_DATA/uploads
-mkdir -p ./_DATA/keycloak-db
+
+# STEP 1: Create necessary files and directories
+# ==============================================
+mkdir -p ./_DATA/{postgres,uploads,keycloak-db}
 
 
 
+
+# STEP 2: Run the stack
+# =====================
+sudo docker network create --subnet=172.18.0.0/24 external
 sudo docker-compose down
 sudo docker-compose up -d --build
